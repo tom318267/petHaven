dfe0d169ee9a949080a6c0df002a8ea4f6f50dcf
tom318267 <ogar318267@gmail.com>
2024-09-25 14:28:38 -0400

    fix errors for vercel

--- src/pages/profile.js
+++
@@ -1,5 +0,0 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-
-export default withPageAuthRequired(function Profile({ user }) {
-  return <div>Hello {user.name}, this is a protected page</div>;
-});
